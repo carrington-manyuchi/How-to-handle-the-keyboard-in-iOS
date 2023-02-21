fc16b060ecb0365745ee9a5a58cb10bee72f592b
Carrington <Manyuchi>
2023-02-21 21:44:35 +0200

    Added the functionality to show and hide the keyboard when necessary.

--- How to handle the keyboard/ViewController.swift
+++ How to handle the keyboard/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtField: UITextField!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var buttonTapped: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureView()
+        
     }
-
+    
+    
+    func configureView() {
+        
+        self.buttonTapped.layer.cornerRadius = 25
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisplay), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        keyboardHidding()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    @objc func keyboardWillDisplay(notification: Notification) {
+        if let keyboardframe:NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardframe.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            self.bottomConstraint.constant = keyboardHeight - 10
+        }
+    }
+    func keyboardHidding() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(keyboardRemoved))
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc func keyboardRemoved() {
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        bottomConstraint.constant = 50
+    }
+    
+    
 
 }
 
